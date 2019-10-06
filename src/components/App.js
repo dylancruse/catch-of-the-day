c002c011a02c95dda1f159e362043ae05a05c7cd
Dylan Cruse <dylancruse@live.com>
2019-10-05 21:11:33 -0500

    Create main App component and Header, Inventory, and Order components

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Header from './Header';
+import Order from './Order';
+import Inventory from './Inventory';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className="catch-of-the-day">
+        <Header />
+        <Order />
+        <Inventory />
+      </div>
+    );
+  }
+}
---
+++ src/components/Inventory.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+export default class Inventory extends Component {
+  render() {
+    return <div className="inventory">Inventory</div>;
+  }
+}
