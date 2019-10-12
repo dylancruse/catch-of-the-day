b3cc9f8cecec8f1a1122a2f54cb9ac5672f2d23a
Dylan Cruse <dylancruse@live.com>
2019-10-11 19:05:34 -0500

    Display order items in Order component

--- src/components/Order.js
+++ src/components/Order.js
@@ -1,16 +1,48 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { formatPrice } from '../helpers';
 
 export default class Order extends Component {
+  renderOrder = key => {
+    const { fishes, order } = this.props;
+    const fish = fishes[key];
+    const count = order[key];
+    const isAvailable = fish.status === 'available';
+    if (!isAvailable) {
+      return (
+        <li key={key}>
+          Sorry {fish ? fish.name : 'fish'} is no longer available 😫
+        </li>
+      );
+    }
+    return (
+      <li key={key}>
+        {count} lbs {fish.name} {formatPrice(count * fish.price)}
+      </li>
+    );
+  };
+
   render() {
     const { fishes, order } = this.props;
     const orderIds = Object.keys(order);
+    const total = orderIds.reduce((prevTotal, key) => {
+      const fish = fishes[key];
+      const count = order[key];
+      const isAvailable = fish && fish.status === 'available';
+      if (isAvailable) {
+        return prevTotal + count * fish.price;
+      }
+      return prevTotal;
+    }, 0);
 
     return (
       <div className="order-wrap">
         <h2>Order</h2>
-        {orderIds}
-        <ul></ul>
+        <ul className="order">{orderIds.map(this.renderOrder)}</ul>
+        <div className="total">
+          Total:
+          <strong>{formatPrice(total)}</strong>
+        </div>
       </div>
     );
   }
